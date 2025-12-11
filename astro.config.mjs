45658087c87f7cd929316073fd4ed3e959b32cff
zbl <zbl@MBP-2FV8ERMD6M-2112.local>
2025-12-11 15:56:30 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://southalone.github.io",
+	base: "/my-log",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
